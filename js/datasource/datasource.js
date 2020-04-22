b198b9f262182a77ec8abd1f4a79f7a06d6bcb6f
Ricardo Caldas <ricardo@ricardombc.com.br>
2020-04-22 13:06:53 -0300

    feature/CRONAPP-1532 - Ajuste para permitir promise no datasource
    * Ajuste para permitir promise no datasource
    
    * gulp
    
    * gulp

--- js/datasource/datasource.js
+++ js/datasource/datasource.js
@@ -695,7 +695,9 @@ angular.module('datasourcejs', [])
                 }
               }
 
-              if (result) {
+              if (result instanceof Promise) {
+                result.then(function(result) {_self.successCallback(result)}).catch(function(reason){_self.handleError(reason)})
+              } else if(result){
                 this.successCallback(result);
               }
 
