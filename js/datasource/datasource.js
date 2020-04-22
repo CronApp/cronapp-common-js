18cd5aba17947ab99c6a5e2c399388f5ab8eafe2
Ricardo Caldas <ricardo@ricardombc.com.br>
2020-04-22 13:02:36 -0300

    gulp

--- js/datasource/datasource.js
+++ js/datasource/datasource.js
@@ -696,7 +696,7 @@ angular.module('datasourcejs', [])
               }
 
               if (result instanceof Promise) {
-                result.then(function(result) {this.successCallback(result)}).catch(function(reason){_self.handleError(reason)})
+                result.then(function(result) {_self.successCallback(result)}).catch(function(reason){_self.handleError(reason)})
               } else if(result){
                 this.successCallback(result);
               }
