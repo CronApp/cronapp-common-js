fe17aa490ac72f63f676c8d24ab5332c86aa78cd
Thiago Costa <thiagoalvescosta@gmail.com>
2020-06-01 11:41:52 -0300

    CRONAPP-1928 - Gulp Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp'),
-  autoprefixer = require('gulp-autoprefixer'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  notify = require('gulp-notify'),
-  uglifycss = require('gulp-uglifycss'),
-  imagemin = require('gulp-imagemin'),
-  htmlmin = require('gulp-htmlmin')
-  ngAnnotate = require('gulp-ng-annotate')
-gulp.task('minify-js', function () {
-    return gulp.src('js/**')
-		.pipe(ngAnnotate())
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js/'));
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify'),
+    uglifycss = require('gulp-uglifycss'),
+    imagemin = require('gulp-imagemin'),
+    htmlmin = require('gulp-htmlmin'),
+    ngAnnotate = require('gulp-ng-annotate-patched'),
+    minify = require("gulp-babel-minify");
+gulp.task('minify-js', function() {
+  return gulp.src(['js/**'])
+      .pipe(ngAnnotate())
+      .pipe(minify({
+        mangle: {
+          keepClassName: true
+        }
+      }))
+      .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('build', ['minify-js']);
